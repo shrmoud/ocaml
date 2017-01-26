2c65de7473652781dd0a3efe91c4534a8bd4c2bd
Ubuntu <ubuntu@ip-172-31-34-84.us-west-2.compute.internal>
2017-01-26 02:52:54 +0000

    Not working?

--- hws/hw2.ml
+++ hws/hw2.ml
@@ -1,6 +1,8 @@
 (* Q1 *)
 let rec factorial (x:int): int =
-   x * (factorial x-1)
+match x with
+| 0 -> 1
+| _ -> x * (factorial x-1)
 ;;
 
 
@@ -28,7 +30,7 @@ let rec selectionSort = function
 let rec add3toList (xs: int list): int list =
    match xs with
    | [] -> []
-   | head::tail -> (head*3):: (add3toList tail)
+   | head::tail -> (head+3):: (add3toList tail)
    ;;
 
 
@@ -38,14 +40,14 @@ let rec fibo (x:int): int =
    match x with
    | 0 -> 1
    | 1 -> 1
-   | _ -> x * (fibo x-1)
+   | _ -> (fibo x-2) * (fibo x-1)
    ;;
 
 
 (* Q6 *)
 let rec countOddList (xs: int list): int = 
    match xs with
-   | [] -> []
+   | [] -> 0
    | head::tail -> if head mod 2 = 0 then 1+(countOddList tail) else 0+(countOddLit tail)
    ;;
 
@@ -53,17 +55,23 @@ let rec countOddList (xs: int list): int =
 (* Q7 *)
 (* Write a function that takes an integer list and return sum of all elements of the list. 
  * If the list is empty then return None.*)
-let rec sumOfList (xs: int list): int options =
+let rec sum (xs: int list): int =
    match xs with
-   | [] -> None
-   | head::tail -> head + (sumOfList tail)
+   | [] -> 0
+   | head::tail -> head + (sum tail)
    ;;
+
+let sumOfList (xs: int list): int option =
+match xs with
+| [] -> None
+| _ -> Some(add xs)
+;;
 
 (* Q8 *)
 (* Write a function that takes a list of int*string. 
  * The function produce another list of string*int using the elements of the given list. *)
 
-let rec makeList (xs: (int * string) list): (string * int) = 
+let rec makeList (xs: (int * string) list): (string * int) list = 
    match xs with
    | [] -> []
    | (x,y) :: tail -> (y,x):: (makeList tail) 
